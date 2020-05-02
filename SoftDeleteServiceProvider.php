529c37490f9471ab4321503f2f1f68de1c74416b
LongDinh <conkhidan@gmail.com>
2020-05-03 00:35:01 +0700

    first commit

---
+++ SoftDeleteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Conkhidan\SoftDeletesFlag;
+
+use Illuminate\Support\ServiceProvider;
+
+class SoftDeleteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
